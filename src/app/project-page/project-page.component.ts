0a2aa3875cb88d60f086fa9cad7037eb8963b6ad
Johnnie Hicks <johnnie432@icloud.com>
2026-08-08 18:48:21 -0400

    Save local progress

---
+++ src/app/project-page/project-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-page',
+  standalone: false,
+  templateUrl: './project-page.component.html',
+  styleUrl: './project-page.component.scss',
+})
+export class ProjectPageComponent {
+
+}
