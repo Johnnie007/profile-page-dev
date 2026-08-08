6d463c21019dc1dc03db71a544a8f9df252c8eea
Johnnie Hicks <johnnie432@icloud.com>
2026-08-08 18:33:41 -0400

    added components

---
+++ src/app/shopping-page/shopping-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-page',
+  standalone: false,
+  templateUrl: './shopping-page.component.html',
+  styleUrl: './shopping-page.component.scss',
+})
+export class ShoppingPageComponent {
+
+}
