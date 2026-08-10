b4e34cc8839556600963f904fb564fc7a6cbd110
Johnnie Hicks <johnnie432@icloud.com>
2026-08-10 18:36:10 -0400

    created model

---
+++ src/app/models/book.ts
@@ -0,0 +1,12 @@
+export interface Book {
+    id: string,
+    series: string,
+    category: string,
+    details: string,
+    cost: string,
+    releaseDate: Date,
+    inventory: number,
+    printToOrderLink: string,
+    pages: number,
+    image: string,
+}
