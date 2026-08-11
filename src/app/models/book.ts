fa7e6294ea60f0e7c84ae60a4891938f4cd5d9ab
Johnnie Hicks <johnnie432@icloud.com>
2026-08-11 13:13:51 -0400

    hardcoded fields for books

--- src/app/models/book.ts
+++ src/app/models/book.ts
@@ -1,12 +1,13 @@
 export interface Book {
     id: string,
+    title: string
     series: string,
     category: string,
-    details: string,
-    cost: string,
-    releaseDate: Date,
-    inventory: number,
-    printToOrderLink: string,
-    pages: number,
-    image: string,
+    details: string | null,
+    cost: string | null,
+    releaseDate: Date | null,
+    inventory: number | null,
+    printToOrderLink: string | null,
+    pages: number | null,
+    image: string | null,
 }
