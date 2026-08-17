882ad6005d8dcd85877c6ef9a2e2099d1ee68ab5
Johnnie Hicks <johnnie432@icloud.com>
2026-08-17 19:46:11 -0400

    moved json in public file and created service class

--- src/app/rest-service/rest-service.service.ts
+++ src/app/rest-service/rest-service.service.ts
@@ -1,9 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Service, inject } from '@angular/core';
+import { Project } from '../models/projects';
+import { Observable } from 'rxjs';
+import { BookSeries } from '../models/book';
 
 @Service()
 export class RestServiceService {
     private http = inject(HttpClient)
-  
+    private projectsUrl="public/books.json";
+    private booksUrl="public/projects.json";
+    
+    getProjects(): Observable<Project[]>{
+        return this.http.get<Project[]>(this.projectsUrl);
+    }
+
+    getBooks(): Observable<BookSeries[]>{
+        return this.http.get<BookSeries[]>(this.booksUrl);
+    }
 }
 
