cc9b3b1c91e4d1f718b9751a43e114324dadba43
Johnnie Hicks <johnnie432@icloud.com>
2026-08-18 16:13:01 -0400

    test

--- src/app/about-page/about-page.component.spec.ts
+++ src/app/about-page/about-page.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutPageComponent } from './about-page.component';
-import { LucideArrowUpRight, LucideBookOpen, LucideOctagonMinus } from '@lucide/angular';
+import { LucideArrowUpRight, LucideBookOpen,LucideActivity } from '@lucide/angular';
 
 describe('AboutPageComponent', () => {
   let component: AboutPageComponent;
